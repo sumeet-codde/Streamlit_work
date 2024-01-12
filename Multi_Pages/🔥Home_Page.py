2eea79b51c7c729d8f6cbe98b6019935c69f0a10
Sumeet Ghodke <66643575+sumeet-codde@users.noreply.github.com>
2024-01-12 18:39:43 +0530

    added config.toml file

--- Multi_Pages/🔥Home_Page.py
+++ Multi_Pages/🔥Home_Page.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import toml
 
 st.set_page_config('Multi_page_Application')
 
 
 
 st.title('MultiPage_application')
-
+config_data = toml.load("config.toml")
 
 if 'input' not in st.session_state:
     st.session_state['input']=""
